3fc0537363493ee05ed9d09e0d2cb2667ee4091d
Ian Nordby <ian.c.nordby@gmail.com>
2023-02-21 11:48:14 -0700

    Refactor fetch call

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,5 +1,6 @@
 const getData = async (updater) => {
   const res = await fetch('http://localhost:3001/api/v1/reservations')
+  console.log(res)
   const data = await res.json()
   updater(data)
   console.log(data)
