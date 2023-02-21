e1183059dffe1c73dc0f89d6790c5388e0c81476
Ian Nordby <ian.c.nordby@gmail.com>
2023-02-21 10:09:48 -0700

    Add From component

---
+++ src/Form/Form.js
@@ -0,0 +1,43 @@
+import React, {useState} from "react";
+
+const Form = ({addRes}) => {
+  const [name, setName] = useState('')
+  const [date, setDate] = useState('')
+  const [time, setTime] = useState('')
+  const [guests, setGuests] = useState('')
+
+  const submitHandler = (event) => {
+    event.preventDefault(event)
+    if(!validateForm()) return
+
+    const postObj = {
+      id: Date.now(), // remove when posting
+      name,
+      date,
+      time,
+      number: +guests
+    }
+
+    addRes(postObj)
+  }
+
+  const validateForm = () => {
+    if(!name || !date || !time || !guests) {
+      // error state change?
+      return false
+    }
+    return true
+  }
+
+  return (
+    <form onSubmit={event => submitHandler(event)}>
+      <input value={name} placeholder='Name' onChange={event => setName(event.target.value)}></input>
+      <input value={date} placeholder='Date (mm/dd)' onChange={event => setDate(event.target.value)}></input>
+      <input value={time} placeholder='Time' onChange={event => setTime(event.target.value)}></input>
+      <input value={guests} placeholder='Number of Guests' onChange={event => setGuests(event.target.value)}></input>
+      <button>Make a Reservation</button>
+    </form>
+  )
+}
+
+export default Form
