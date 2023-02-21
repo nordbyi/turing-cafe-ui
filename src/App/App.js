a6d1ef89a8910ceaa9646443ecb26914d77282b1
Ian Nordby <ian.c.nordby@gmail.com>
2023-02-21 10:09:09 -0700

    Add components

--- src/App/App.js
+++ src/App/App.js
@@ -1,20 +1,31 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
+import Reservations from '../Reservations/Reservations.js';
+import Form from '../Form/Form.js';
+import getData from '../apiCalls.js';
 import './App.css';
 
-class App extends Component {
-  render() {
+const App = () => {
+    const [reservations, setReservations] = useState([])
+
+    useEffect(() => {
+      getData(setReservations)
+    }, [])
+
+    const addReservation = (newReservation) => {
+      setReservations([...reservations, newReservation])
+    }
+
     return (
       <div className="App">
         <h1 className='app-title'>Turing Cafe Reservations</h1>
         <div className='resy-form'>
-
+          <Form addRes={addReservation}/>
         </div>
         <div className='resy-container'>
-          
+          <Reservations reservs={reservations}/>
         </div>
       </div>
     )
-  }
 }
 
 export default App;
